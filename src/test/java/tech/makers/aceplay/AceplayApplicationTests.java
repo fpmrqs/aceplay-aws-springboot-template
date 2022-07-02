c81ce7d7fae7d18426abfe38bf9730a8994efd85
Ryan Pierre <info@ryanpierre.com>
2022-07-02 19:53:13 +0100

    update vscode settings to use the right test config

--- src/test/java/tech/makers/aceplay/AceplayApplicationTests.java
+++ src/test/java/tech/makers/aceplay/AceplayApplicationTests.java
@@ -2,9 +2,11 @@ package tech.makers.aceplay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+// @RunWith(SpringRunner.class)
 @SpringBootTest
 class AceplayApplicationTests {
 
