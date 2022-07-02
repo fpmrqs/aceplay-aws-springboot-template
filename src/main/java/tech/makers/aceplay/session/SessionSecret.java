4e97fddf755a5891477a6093d780616c5279c157
Ryan Pierre <info@ryanpierre.com>
2022-07-02 21:18:05 +0100

    all tests passing:

--- src/main/java/tech/makers/aceplay/session/SessionSecret.java
+++ src/main/java/tech/makers/aceplay/session/SessionSecret.java
@@ -18,8 +18,8 @@ public class SessionSecret {
   public SessionSecret(
       @Value("${spring.profiles.active:unknown}") String activeProfile,
       @Value("${jwt.token.secret:@null}") String secretKeyBase64Encoded) {
-      SessionSecret.activeProfile = activeProfile;
-      SessionSecret.secretKeyBase64Encoded = secretKeyBase64Encoded;
+      this.activeProfile = activeProfile;
+      this.secretKeyBase64Encoded = secretKeyBase64Encoded;
   }
 
   public static SecretKey getKey() {
